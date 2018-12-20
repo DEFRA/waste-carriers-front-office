0e9f4ea5edfabe1db064c8a11f5545607e31d797
irisfaraway <iris.faraway@gmail.com>
2018-12-20 13:45:56 +0000

    Order list by date registered
    This replicates the current behaviour in the frontend.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,6 +5,7 @@ class DashboardsController < ApplicationController
 
   def index
     @registrations = WasteCarriersEngine::Registration.where(account_email: current_user.email)
+                                                      .order_by("metaData.dateRegistered": :asc)
                                                       .page(params[:page])
   end
 end
