1918ddad0e78baf8d18e83d428c2d3ebea2f0f45
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2019-12-27 16:16:50 +0000

    Add config for the mock delay
    We actually have an existing environment variable we use for setting the delay to use with the existing mocking functionality.
    
    Should we come to want to continue to use it, the initializer will need to look for and pull values from the env var, as just use the default built into the mock engine.

--- config/initializers/defra_ruby_mocks.rb
+++ config/initializers/defra_ruby_mocks.rb
@@ -4,4 +4,7 @@ DefraRubyMocks.configure do |configuration|
   # Enable the mock routes mounted in this app if the environment is configured
   # for it
   configuration.enable = ENV["WCRS_MOCK_ENABLED"] || false
+  # Set how long the mock should delay before responding. In the engine itself
+  # the default is 1000ms (1 second)
+  configuration.delay = ENV["WCRS_MOCK_DELAY"] || 1000
 end
