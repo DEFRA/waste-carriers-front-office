510e14d726f563631a60b52dbd0a79336cd57db3
Toby Privett <toby@snaplab.co.uk>
2021-08-02 12:08:55 +0100

    Allow users to update cookie settings after initial selection
    https://eaflood.atlassian.net/browse/RUBY-1536

--- app/controllers/cookies_controller.rb
+++ app/controllers/cookies_controller.rb
@@ -2,26 +2,44 @@
 
 class CookiesController < ApplicationController
   def accept_analytics
-    write_cookie_and_redirect(:cookies_policy, :analytics_accepted)
+    write_cookie(:cookies_policy, :analytics_accepted)
+    redirect_back fallback_location: "/"
   end
 
   def reject_analytics
-    write_cookie_and_redirect(:cookies_policy, :analytics_rejected)
+    write_cookie(:cookies_policy, :analytics_rejected)
+    redirect_back fallback_location: "/"
   end
 
   def hide_this_message
-    write_cookie_and_redirect(:cookies_preferences_set, true)
+    write_cookie(:cookies_preferences_set, true)
+    redirect_back fallback_location: "/"
+  end
+
+  def update
+    # Google writes the cookies as ".hostname"
+    # so we need to state the domain when removing
+    cookies.to_hash.each_pair do |k, _v|
+      cookies.delete k, domain: ".#{request.hostname}"
+    end
+
+    if params[:analytics] == "accept"
+      write_cookie(:cookies_policy, :analytics_accepted)
+    else
+      write_cookie(:cookies_policy, :analytics_rejected)
+    end
+    write_cookie(:cookies_preferences_set, true)
+
+    redirect_to "/fo/pages/cookies?cookies_updated=true"
   end
 
   protected
 
-  def write_cookie_and_redirect(name, value)
+  def write_cookie(name, value)
     cookies[name] =
       {
         value: value,
         expires: 1.year
       }
-
-    redirect_back fallback_location: root_path
   end
 end
