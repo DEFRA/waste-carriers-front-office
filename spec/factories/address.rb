cff40c455035e0d07507d1ca0cca9d9b63f48fea
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2022-10-04 11:58:24 +0100

    Fix address lines (#943)
    https://eaflood.atlassian.net/browse/RUBY-2142

--- spec/factories/address.rb
+++ spec/factories/address.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :address, class: "WasteCarriersEngine::Address" do
     house_number { "42" }
-    address_line1 { "Foo Gardens" }
+    address_line_1 { "Foo Gardens" }
     town_city { "Baz City" }
     postcode { "FA1 1KE" }
     uprn { "340116" }
