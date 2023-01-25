a30cffa201e440933b72e8e306f2c159d70f87f2
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2023-01-25 16:14:42 +0000

    Chore/covid grace window (#1004)
    https://eaflood.atlassian.net/browse/RUBY-2182

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class SessionsController < ::Devise::SessionsController
+class SessionsController < Devise::SessionsController
   def destroy
     current_user.invalidate_all_sessions!
     super
