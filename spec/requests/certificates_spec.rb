4cf590244c7fc9a24b3751e44d1fe5d8f7735f9e
Jerome Pratt <jerome.pratt@defra.gov.uk>
2023-07-24 10:40:44 +0100

    Spec fixes to reflect user not being redirected when not logged in

--- spec/requests/certificates_spec.rb
+++ spec/requests/certificates_spec.rb
@@ -7,41 +7,40 @@ RSpec.describe "Certificates" do
   let(:registration) { create(:registration, :expires_soon, account_email: user.email) }
 
   describe "GET /fo/registrations/:reg_identifier/certificate/pdf" do
-
     before { allow(WasteCarriersEngine::FeatureToggle).to receive(:active?).with(:block_front_end_logins).and_return false }
 
-      context "when the user who owns the registration is logged in" do
-        before { sign_in(user) }
+    context "when the user who owns the registration is logged in" do
+      before { sign_in(user) }
 
-        it "responds with a PDF with a filename that includes the registration reference" do
-          get "/fo/registrations/#{registration.reg_identifier}/certificate/pdf"
+      it "responds with a PDF with a filename that includes the registration reference" do
+        get "/fo/registrations/#{registration.reg_identifier}/certificate/pdf"
 
-          expect(response.content_type).to eq("application/pdf")
-          expected_content_disposition = "inline; filename=\"#{registration.reg_identifier}.pdf\"; filename*=UTF-8''#{registration.reg_identifier}.pdf"
-          expect(response.headers["Content-Disposition"]).to eq(expected_content_disposition)
+        expect(response.content_type).to eq("application/pdf")
+        expected_content_disposition = "inline; filename=\"#{registration.reg_identifier}.pdf\"; filename*=UTF-8''#{registration.reg_identifier}.pdf"
+        expect(response.headers["Content-Disposition"]).to eq(expected_content_disposition)
+      end
+
+      it "returns a 200 status code" do
+        get "/fo/registrations/#{registration.reg_identifier}/certificate/pdf"
+        expect(response).to have_http_status(:ok)
+      end
+
+      context "when the 'show_as_html' query string is present" do
+        context "when the value is 'true'" do
+          it "responds with HTML" do
+            get "/fo/registrations/#{registration.reg_identifier}/certificate?show_as_html=true"
+            expect(response.content_type).to eq("text/html; charset=utf-8")
+          end
         end
 
-        it "returns a 200 status code" do
-          get "/fo/registrations/#{registration.reg_identifier}/certificate/pdf"
-          expect(response).to have_http_status(:ok)
+        [false, 1, 0, :foo].each do |bad_value|
+          context "when the value is '#{bad_value}'" do
+            it "responds with a PDF" do
+              get "/fo/registrations/#{registration.reg_identifier}/certificate/pdf?show_as_html=#{bad_value}"
+              expect(response.content_type).to eq("application/pdf")
+            end
+          end
         end
-
-        context "when the 'show_as_html' query string is present" do
-          context "when the value is 'true'" do
-            it "responds with HTML" do
-              get "/fo/registrations/#{registration.reg_identifier}/certificate?show_as_html=true"
-              expect(response.content_type).to eq("text/html; charset=utf-8")
-            end
-          end
-
-          [false, 1, 0, :foo].each do |bad_value|
-            context "when the value is '#{bad_value}'" do
-              it "responds with a PDF" do
-                get "/fo/registrations/#{registration.reg_identifier}/certificate/pdf?show_as_html=#{bad_value}"
-                expect(response.content_type).to eq("application/pdf")
-              end
-            end
-          end
       end
     end
 
@@ -58,6 +57,15 @@ RSpec.describe "Certificates" do
       it "redirects to the sign-in page" do
         get "/fo/registrations/#{registration.reg_identifier}/certificate/pdf"
         expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+    context "when the :block_front_end_logins feature toggle is active" do
+      before { allow(WasteCarriersEngine::FeatureToggle).to receive(:active?).with(:block_front_end_logins).and_return true }
+
+      it "does not redirect to the application root" do
+        get "/fo/registrations/#{registration.reg_identifier}/certificate/pdf"
+        expect(response).not_to redirect_to(root_path)
       end
     end
   end
@@ -78,12 +86,4 @@ RSpec.describe "Certificates" do
     end
   end
 
-  context "when the :block_front_end_logins feature toggle is active" do
-    before { allow(WasteCarriersEngine::FeatureToggle).to receive(:active?).with(:block_front_end_logins).and_return true }
-
-    it "redirects to the application root" do
-      get "/fo/registrations/#{registration.reg_identifier}/certificate"
-      expect(response).to redirect_to(root_path)
-    end
-  end
 end
