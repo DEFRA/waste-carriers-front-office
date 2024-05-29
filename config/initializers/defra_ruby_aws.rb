2361a53ee0f825593aa08034f435979cebe2255a
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2024-05-29 15:29:10 +0100

    Add default AWS region

--- config/initializers/defra_ruby_aws.rb
+++ config/initializers/defra_ruby_aws.rb
@@ -5,7 +5,7 @@ require "defra_ruby/aws"
 DefraRuby::Aws.configure do |c|
   govpay_mocks_bucket = {
     name: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_BUCKET", nil),
-    region: ENV.fetch("AWS_REGION", nil),
+    region: ENV.fetch("AWS_REGION", "eu-west-1"),
     credentials: {
       access_key_id: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_ACCESS_KEY_ID", "dummy"),
       secret_access_key: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_SECRET_ACCESS_KEY", "dummy")
