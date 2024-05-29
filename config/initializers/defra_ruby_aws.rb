4a0af96af0a55d8921edb327eab661a443c77d1a
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2024-05-29 14:24:56 +0100

    Add AWS config for mocks and adjust govpay mocks config

---
+++ config/initializers/defra_ruby_aws.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require "defra_ruby/aws"
+
+DefraRuby::Aws.configure do |c|
+  govpay_mocks_bucket = {
+    name: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_BUCKET", nil),
+    region: ENV.fetch("AWS_REGION", nil),
+    credentials: {
+      access_key_id: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_ACCESS_KEY_ID", "dummy"),
+      secret_access_key: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_SECRET_ACCESS_KEY", "dummy")
+    },
+    encrypt_with_kms: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_ENCRYPT_WITH_KMS", nil)
+  }
+
+  c.buckets = [govpay_mocks_bucket]
+end
