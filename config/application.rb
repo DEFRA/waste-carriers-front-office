17e3e8c30c2067eec3b1b949e27e450de8ee6e4c
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2024-09-10 16:39:21 +0100

    Fix/ruby 3330 duplicate text (#1376)
    https://eaflood.atlassian.net/browse/RUBY-3330

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module WasteCarriersFrontOffice
     config.time_zone = "UTC"
 
     # The default locale is :en and all translations from config/locales/*/*.rb,yml are auto loaded.
-    config.i18n.load_path += Dir[Rails.root.join("config/locales/**/*.{rb,yml}")]
+    config.i18n.load_path += Rails.root.glob("config/locales/**/*.{rb,yml}")
     # config.i18n.default_locale = :de
 
     # Enable the asset pipeline
