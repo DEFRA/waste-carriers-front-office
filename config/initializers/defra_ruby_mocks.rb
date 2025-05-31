4decf1e6bca482c0649d4f2bbc413c16e9defa98
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2025-05-31 12:52:43 +0100

    Configure host_is_back_office for mocks engine

--- config/initializers/defra_ruby_mocks.rb
+++ config/initializers/defra_ruby_mocks.rb
@@ -4,10 +4,14 @@ DefraRubyMocks.configure do |configuration|
   # Enable the mock routes mounted in this app if the environment is configured
   # for it
   configuration.enable = ENV["WCRS_MOCK_ENABLED"] || false
+
   # Set how long the mock should delay before responding. In the engine itself
   # the default is 1000ms (1 second)
   configuration.delay = ENV["WCRS_MOCK_DELAY"] || 1000
 
   # Govpay API mock details. Note FO application point to BO mocks and vice-versa by defafult.
   configuration.govpay_domain = ENV["WCRS_MOCK_FO_GOVPAY_URL"] || "http://localhost:8001/bo/mocks/govpay/v1"
+
+  # BO mocks point at FO and vice-versa; some mock need to know which to point at.
+  configuration.host_is_back_office = false
 end
