c8f62d83219995732698f630f8848b598a341a02
Leonid Brujev <brujev@gmail.com>
2026-09-08 11:59:09 +0100

    Upgrade Brakeman to version 8.0.6 and adjust environment configurations for Rails 8.1 compatibility

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,6 @@ Bundler.require(*Rails.groups)
 module WasteCarriersFrontOffice
   class Application < Rails::Application
     config.load_defaults 6.1
-    config.autoloader = :classic
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
